ca124ecc41e224c64dbbacc5c9beca4b9bf47bdb
Lindan Maulana <lindanstemga@gmail.com>
2024-12-12 12:16:52 +0700

    add lancuhing

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
